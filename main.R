cff1ab8bd8a703fe51d469c6809c35949672eddb
Joli Holmes <holmesjoli@gmail.com>
2020-07-08 14:10:25 -0500

    simiplifed main file by calling functions

--- main.R
+++ main.R
@@ -7,24 +7,22 @@ config <- yaml::read_yaml("config.yaml")
 
 cnty <- readr::read_csv(here::here("./data/tx_counties.csv"))
 
-occ_xwalk <- dm.occ_census_datausaio(url = config$occ, cnty_fips = cnty$county_fips, table = "S2401")
+occ_xwalk <- dm.occ_census_datausaio(url = config$occ,
+                                     cnty_fips = cnty$county_fips,
+                                     table = "S2401")
 
-tables <- list(ind = "S2405", occ = "S2401")
+ind_xwalk <- dm.ind_census_datausaio(url = config$ind,
+                                     cnty_fips = cnty$county_fips,
+                                     table = "S2403")
 
-l <- sapply(tables, scrape_html, USE.NAMES = TRUE, simplify = FALSE)
+occ <- census_call(id_vars = c("TRACT", "COUNTY"),
+                   value_vars = occ_xwalk$var,
+                   key = key$census) %>%
+  dplyr::select(-c(tract, county, state)) %>% 
+  tidyr::gather(var, value, -c(TRACT, COUNTY))
 
-dfs <- sapply(l, function(x, key) {
-  census_call(id_vars = c("TRACT", "COUNTY"),
-              value_vars = x$Name,
-              key = key)
-  }, key = key$census, USE.NAMES = TRUE, simplify = FALSE)
-
-ind_xwalk <- create.ind_xwalk(url = config$ind, cnty_fips = cnty$county_fips)
-
-occ_xwalk <- create.occ_xwalk(url = config$occ, cnty_fips = cnty$county_fips)
-
-ind_census_vars <- l[[1]]
-
-
-
-
+ind <- census_call(id_vars = c("TRACT", "COUNTY"),
+                   value_vars = ind_xwalk$var,
+                   key = key$census) %>% 
+  dplyr::select(-c(tract, county, state)) %>% 
+  tidyr::gather(var, value, -c(TRACT, COUNTY))
