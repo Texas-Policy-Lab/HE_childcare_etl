e98191bc02a0a997e1cc8f19fe7bd5cf92ae3ee9
Joli Holmes <holmesjoli@gmail.com>
2020-07-08 09:47:16 -0500

    added code to create industry/occupation crosswalks

--- main.R
+++ main.R
@@ -3,6 +3,9 @@ library(magrittr)
 sapply(list.files("R", full.names = TRUE, recursive = TRUE), source, .GlobalEnv)
 
 key <- yaml::read_yaml("api_key.yaml")
+config <- yaml::read_yaml("config.yaml")
+
+cnty <- readr::read_csv(here::here("./data/tx_counties.csv"))
 
 tables <- list("S2405", "S2401")
 
@@ -13,3 +16,13 @@ dfs <- lapply(l, function(x) {
               value_vars = x$Name,
               key = key$census)
   })
+
+ind <- datausaio(url = config$ind, cnty_fips = cnty$county_fips)
+
+occ <- datausaio(url = config$occ, cnty_fips = cnty$county_fips)
+
+ind_xwalk <- ind %>% 
+  dplyr::distinct(`ID Group`, Group, `ID Industry`, Industry)
+
+occ_xwalk <- occ %>%
+  dplyr::distinct(`ID Group`, Group, `ID Subgroup`, Subgroup, `ID Occupation`, Occupation)
