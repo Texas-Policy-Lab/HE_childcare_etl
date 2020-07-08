4135b15082ce822d840e5d8d6a41a67f62574d80
Joli Holmes <holmesjoli@gmail.com>
2020-07-08 14:22:28 -0500

    added the phased reopening percentages

--- main.R
+++ main.R
@@ -7,6 +7,12 @@ config <- yaml::read_yaml("config.yaml")
 
 cnty <- readr::read_csv(here::here("./data/tx_counties.csv"))
 
+occ_reopening <- readr::read_csv("./data/occupation_reopening_pct.csv") %>% 
+  tidyr::gather(phase, pct, -c(occ, occ_id))
+
+ind_reopening <- readr::read_csv("./data/industry_reopening_pct.csv") %>% 
+  tidyr::gather(phase, pct, -c(ind, ind_id))
+
 occ_xwalk <- dm.occ_census_datausaio(url = config$occ,
                                      cnty_fips = cnty$county_fips,
                                      table = "S2401")
@@ -19,10 +25,15 @@ occ <- census_call(id_vars = c("TRACT", "COUNTY"),
                    value_vars = occ_xwalk$var,
                    key = key$census) %>%
   dplyr::select(-c(tract, county, state)) %>% 
-  tidyr::gather(var, value, -c(TRACT, COUNTY))
+  tidyr::gather(var, n_workers, -c(TRACT, COUNTY)) %>% 
+  dplyr::left_join(occ_xwalk) %>% 
+  dplyr::left_join(occ_reopening)
+  
 
 ind <- census_call(id_vars = c("TRACT", "COUNTY"),
                    value_vars = ind_xwalk$var,
                    key = key$census) %>% 
   dplyr::select(-c(tract, county, state)) %>% 
-  tidyr::gather(var, value, -c(TRACT, COUNTY))
+  tidyr::gather(var, n_workers, -c(TRACT, COUNTY)) %>% 
+  dplyr::left_join(ind_xwalk) %>% 
+  dplyr::left_join(ind_reopening)
