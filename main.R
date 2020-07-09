789b554dacf0536f5bcb2581d4949012aeac8812
Joli Holmes <holmesjoli@gmail.com>
2020-07-09 17:36:02 -0500

    moved main data management steps from R/osrm_demo.R

---
+++ main.R
@@ -0,0 +1,50 @@
+library(osrm)
+library(magrittr)
+
+getOption("osrm.server")
+options(osrm.server = "http://127.0.0.1:5000/",
+        osrm.profile = "driving")
+
+config <- yaml::read_yaml("config.yaml")
+
+sapply(list.files("R", full.names = TRUE, recursive = TRUE), source, .GlobalEnv)
+
+df <- readr::read_csv(file.path(config$data_pth, "family_zip_prvdr.csv")) %>%
+  dplyr::rename(parent_id = ParentsID) %>% 
+  tidyr::drop_na(ParentsID) %>%
+  dplyr::mutate(familyzip = stringr::str_pad(as.character(Parents.FamilyZip)),
+                width = 5, side = "left", pad = "0")
+
+assertthat::assert_that(sum(is.na(df$Parents.FamilyZip)) == 0)
+
+temp <- readr::read_tsv(file.path(config$data_pth, "2019_Gaz_zcta_national.txt")) %>%
+  dplyr::mutate(familyzip = as.character(GEOID))
+
+df <- df %>%
+  dplyr::left_join(temp %>% 
+                     dplyr::select(familyzip, INTPTLAT, INTPTLONG), by = c("familyzip"))
+
+a <- df %>%
+  dplyr::distinct(operation_number, .keep_all=TRUE) %>%
+  dplyr::group_by(operation_number) %>%
+  dplyr::select(operation_number, longitude, latitude) %>%
+  dplyr::group_split()
+
+names(a) <- a  %>% 
+  purrr::map(magrittr::extract("operation_number"))
+
+b <- df %>%
+  dplyr::group_by(operation_number) %>%
+  dplyr::select(operation_number, parent_id, INTPTLONG, INTPTLAT) %>%
+  dplyr::group_split(.keep=FALSE)
+
+names(b) <- names(a)
+
+assertthat::assert_that(length(a) == length(b))
+
+result <- lapply(names(a)[1:5], function(x) {osrm::osrmTable(src = a[[x]],
+                                                             dst = b[[x]])})
+
+parse_result <- lapply(result, parse_osrm_table_result)
+
+df <- do.call("rbind", parse_result)
