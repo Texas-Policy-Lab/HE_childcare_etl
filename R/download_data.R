5784a7d82556116c338bad02651410cabe3d9b5e
Joli Holmes <jh111@ad.rice.edu>
2020-07-15 11:46:39 -0500

    renamed parameter

--- R/download_data.R
+++ R/download_data.R
@@ -35,10 +35,10 @@ unlist_hhsc_ccl_data <- function(c) {
 
 #' @title Returns the most recent HHSC CLL Daycare and Residential Operations Data
 #' @description Link to data: https://data.texas.gov/Social-Services/HHSC-CCL-Daycare-and-Residential-Operations-Data/bc5r-88dy/data
-#' @param data_name string. The name to of the data to read in.
+#' @param data_in_name string. The name to of the data to read in.
 #' @param data_in_pth string. The path to read the data in from.
 #' @export
-get.hhsc_ccl_data <-  function(data_name,
+get.hhsc_ccl_data <-  function(data_in_name,
                                data_in_pth,
                                url = "https://data.texas.gov/api/odata/v4/bc5r-88dy",
                                df_all = data.frame()) {
@@ -74,42 +74,42 @@ get.hhsc_ccl_data <-  function(data_name,
   df_all <- sapply(result$df_all, as.character) %>% 
     as.data.frame(stringsAsFactors = FALSE)
   
-  write.csv(df_all, file.path(data_in_pth, data_name), row.names = FALSE)
+  write.csv(df_all, file.path(data_in_pth, data_in_name), row.names = FALSE)
 
 }
 
 #' @title Get NBER Tract data
 #' @description Link to data: http://data.nber.org/distance/2010/sf1/tract/sf12010tractdistance25miles.csv
 #' @details We're using the 25 mile radius rather then the 5 miles radius because the 5 mile radius was missing two counties in Harris County
-#' @param data_name string. The name of the file to download.
+#' @param data_in_name string. The name of the file to download.
 #' @param data_in_pth string. The path to read the data in from.
 #' @export 
-get.nber_tract_data <- function(data_name,
+get.nber_tract_data <- function(data_in_name,
                                 data_in_pth,
                                 url = "http://data.nber.org/distance/2010/sf1/tract/{fl}") {
 
-  dwnld_pth <- file.path(data_in_pth, data_name)
+  dwnld_pth <- file.path(data_in_pth, data_in_name)
 
-  download.file(glue::glue(url, fl = data_name), dwnld_pth)
+  download.file(glue::glue(url, fl = data_in_name), dwnld_pth)
 }
 
 #' @title Get ACF data
 #' @description Link to data: https://www.twc.texas.gov/programs/childcare#dataAndReports
-#' @param data_name string. The name to of the data to read in.
+#' @param data_in_name string. The name to of the data to read in.
 #' @param data_in_pth string. The path to read the data in from.
 #' @export
-get.acf_data <- function(data_name,
+get.acf_data <- function(data_in_name,
                          data_in_pth,
                          url = "https://www.twc.texas.gov/files/partners/{fl}",
                          qtr = 1:4) {
 
   lapply(qtr, function(q) {
 
-    fl_name <- glue::glue(data_name, qtr = q)
+    fl_name <- glue::glue(data_in_name, qtr = q)
     
     dwnld_pth <- file.path(data_in_pth, fl_name)
 
-    download.file(glue::glue(url, fl = fl_name), dwnld_pth)
+    download.file(glue::glue(url, fl = fl_name), destfile = dwnld_pth, mode = "wb")
 
   })
 
@@ -117,10 +117,10 @@ get.acf_data <- function(data_name,
 
 #' @title Get the neighborhood to census tract data
 #' @description Download the neighbordhood to census tract cross walk create by the Kinder Institute. https://www.arcgis.com/apps/MapSeries/index.html?appid=95320b06677c438d91027cb5feb241bf
-#' @param data_name string. The name to of the data to read in.
+#' @param data_in_name string. The name to of the data to read in.
 #' @param data_in_pth string. The path to read the data in from.
 #' @export
-get.kinder_neighborhood_tract_xwalk <- function(data_name,
+get.kinder_neighborhood_tract_xwalk <- function(data_in_name,
                                                 data_in_pth,
                                                 pth = "https://www.datahouston.org/cta_crosswalk.txt") {
 
@@ -129,15 +129,15 @@ get.kinder_neighborhood_tract_xwalk <- function(data_name,
     dplyr::rename_all(tolower) %>%
     dplyr::select(-id)
 
-  write.csv(df, file.path(data_in_pth, data_name), row.names = FALSE)
+  write.csv(df, file.path(data_in_pth, data_in_name), row.names = FALSE)
 }
 
 #' @title Get tract by latitude and longitude
 #' @description Downloads shape file for Harris County (201) Texas (48) using the tigris package, which pulls the most recent shape from the United States Census Bureau.
-#' @param data_name string. The name to of the data to read in.
+#' @param data_in_name string. The name to of the data to read in.
 #' @param data_in_pth string. The path to read the data in from.
 #' @export
-get.tract_shape <- function(data_name,
+get.tract_shape <- function(data_in_name,
                             data_in_pth,
                             state_fips = 48,
                             county_fips = 201) {
@@ -150,15 +150,15 @@ get.tract_shape <- function(data_name,
     dplyr::rename_all(tolower) %>% 
     dplyr::rename(anchor_tract = geoid)
 
-  write.csv(geo, file.path(data_in_pth, data_name), row.names = FALSE)
+  write.csv(geo, file.path(data_in_pth, data_in_name), row.names = FALSE)
 }
 
 #' @title Get State FIPS and State Name Crosswalk
 #' @description Downloads the crosswalk between county fips codes and county names for Texas
-#' @param data_name string. The name to of the data to read in.
+#' @param data_in_name string. The name to of the data to read in.
 #' @param data_in_pth string. The path to read the data in from.
 #' @export
-get.state_fips_state_name_xwalk <- function(data_name,
+get.state_fips_state_name_xwalk <- function(data_in_name,
                                             data_in_pth,
                                             state_fips = 48) {
 
@@ -173,14 +173,14 @@ get.state_fips_state_name_xwalk <- function(data_name,
 
   assertthat::assert_that(nrow(cnty) == 254)
 
-  write.csv(cnty, file.path(data_in_pth, data_name), row.names = FALSE)
+  write.csv(cnty, file.path(data_in_pth, data_in_name), row.names = FALSE)
 }
 
 #' @title Get ZIP shape
-#' @param data_name string. The name to of the data to read in.
+#' @param data_in_name string. The name to of the data to read in.
 #' @param data_in_pth string. The path to read the data in from.
 #' @export
-get.zip_shape <- function(data_name,
+get.zip_shape <- function(data_in_name,
                           data_in_pth,
                           state = 48) {
 
--- download_data_main.R
+++ download_data_main.R
@@ -5,22 +5,22 @@ sapply(list.files("R", full.names = TRUE, recursive = TRUE), source, .GlobalEnv)
 
 data_in_pth <- config$data_pths$in_pth
 
-get.nber_tract_data(data_name = config$data_names$nber,
+get.nber_tract_data(data_in_name = config$data_in_names$nber,
                     data_in_pth = data_in_pth)
 
-get.hhsc_ccl_data(data_name = config$data_names$ccl,
+get.hhsc_ccl_data(data_in_name = config$data_in_names$ccl,
                   data_in_pth = data_in_pth)
 
-get.acf_data(data_name = config$data_names$acf,
+get.acf_data(data_in_name = config$data_in_names$acf,
              data_in_pth = data_in_pth)
 
-get.kinder_neighborhood_tract_xwalk(data_name = config$data_names$kinder_neighborhoods,
+get.kinder_neighborhood_tract_xwalk(data_in_name = config$data_in_names$kinder_neighborhoods,
                                     data_in_pth = data_in_pth)
 
-get.tract_shape(data_name = config$data_names$tract_shape,
+get.tract_shape(data_in_name = config$data_in_names$tract_shape,
                 data_in_pth = data_in_pth)
 
-get.state_fips_state_name_xwalk(data_name = config$data_names$state_fips_state_name_xwalk,
+get.state_fips_state_name_xwalk(data_in_name = config$data_in_names$state_fips_state_name_xwalk,
                                 data_in_pth)                              
              
  
