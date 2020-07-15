f6efab7f16f3e5c0d3a6ae559062d8dde796e602
Joli Holmes <jh111@ad.rice.edu>
2020-07-15 11:13:53 -0500

    added crosswalk tracts dataframe

--- R/manage_data.R
+++ R/manage_data.R
@@ -1,3 +1,40 @@
+#' @title Tracts crosswalk
+#' @description Creates a crosswalk between anchor tracts in harris county and surround tracts within 3 miles of each anchor tract
+#' @param data_name string. The name to of the data to read in.
+#' @param data_in_pth string. The path to read the data in from.
+#' @param data_out_pth string. The path to wrtie the data out to.
+#' @export
+dm.tracts_xwalk <- function(data_name,
+                            data_in_pth,
+                            data_out_pth,
+                            harris_fips = "48201") {
+
+  df <- readr::read_csv(file.path(data_in_pth, data_name))
+
+  assertthat::assert_that(typeof(df$county1) == "character")
+
+  df <- df %>% 
+    dplyr::filter(county1 == harris_fips) %>% 
+    dplyr::mutate(anchor_tract = paste0(county1, tract1),
+                  surround_tract = paste0(county2, tract2))
+
+  tracts <- df %>% 
+    dplyr::distinct(anchor_tract) %>% 
+    dplyr::mutate(surround_tract = anchor_tract)
+  
+  assertthat::assert_that(nrow(tracts) == 786)
+  
+  df <- df %>% 
+    dplyr::filter(mi_to_tract <= 3) %>% 
+    dplyr::bind_rows(tracts) %>% 
+    dplyr::select(anchor_tract, surround_tract)
+
+  write.csv(df, file.path(data_out_pth, "tracts_xwalk.csv"), row.names = FALSE)
+
+  return(df)
+}
+
+
 #' @title Geocode addresses
 #' @descrption Geocodes addresses using the Geocodio API
 #' @param key string. The api key registered with your personal Geocodio account.
