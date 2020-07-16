9aac0d6c0307fc2c8f2ae5f075ab60a810424a11
Joli Holmes <jh111@ad.rice.edu>
2020-07-16 16:27:14 -0500

    removed old code

--- manage_data_main.R
+++ manage_data_main.R
@@ -21,26 +21,9 @@ acf <- dm.merge_acf(data_in_name = config$data_in_names$acf,
 ccl <- dm.ccl(ccl_data_in_pth = data_in_pth,
               ccl_data_in_name = config$data_in_names$ccl,
               ccl_data_out_pth = data_out_pth,
-              ccl_data_out_name = config$data_out_names$ccl)
-
-lat_long <- dm.geocode_address(addresses = ccl$location_address,
-                               key = api_key$mapquest$key)
-
-write.csv(lat_long, "mapquest_geocode.csv", row.names = FALSE)
-
-ccl_address <- ccl %>%
-  dplyr::filter(!is.na(latitude) & !is.na(longitude)) %>%
-  dplyr::select(operation_number, latitude, longitude)
-
-latLng <- lapply(1:nrow(ccl_address), function(add, df) {
-  
-  list(lat = df$latitude[add],
-       lng = df$longitude[add])
-
-}, df = ccl_address)
-
-x <- dm.reverse_geocode(latLng = latLng,
-                        key = api_key$mapquest$key)
+              ccl_data_out_name = config$data_out_names$ccl,
+              geocode = TRUE,
+              key = api_key$mapquest$key)
 
 family_prvdr_zip <- dm.family_prvdr_zip(acf_data_in_pth = data_out_pth,
                                         acf_data_in_name = config$data_out_names$acf_all,
